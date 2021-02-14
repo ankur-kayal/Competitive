c3a4be2800a59d6debd1f184e85f9e616e8e916b
Ankur Kayal <ankurkayal1234@gmail.com>
2021-02-14 15:29:14 +0530

    NTT template added and starter now includes custom hash

--- GetGood/templates/ankur/starter.cpp
+++ GetGood/templates/ankur/starter.cpp
@@ -54,6 +54,35 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 //----------------------------------- END DEFINES -------------------------------- 
 
+//-------------------------- CUSTOM UNORDERED MAP HASH ---------------------------
+
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(a + FIXED_RANDOM);
+    }
+    template<class T> size_t operator()(T a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        return splitmix64(x(a) + FIXED_RANDOM);
+    }
+    template<class T, class H> size_t operator()(pair<T, H> a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        hash<H> y;
+        return splitmix64(x(a.f) * 37 + y(a.s) + FIXED_RANDOM);
+    }
+};
+template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
+
+//----------------------- CUSTOM UNORDERED MAP HASH END--------------------------
+
 void run_cases() {
 
 }
