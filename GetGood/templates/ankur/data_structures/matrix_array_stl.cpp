25e24b40bb5244912a8ed06595e4cc3976add5de
Ankur Kayal <ankurkayal1234@gmail.com>
2021-04-22 20:51:48 +0530

    templates updated

--- GetGood/templates/ankur/data_structures/matrix_array_stl.cpp
+++ GetGood/templates/ankur/data_structures/matrix_array_stl.cpp
@@ -1,116 +1,160 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<class T, int N>
+template<typename T, int N>
 struct Matrix {
-    typedef Matrix M;
 
-    array<array<T, N>,N> values{};
+    static const uint64_t CUTOFF = 15000000180000000540;
+    array<array<T, N>, N> values;
+
+    Matrix() {
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                values[i][j] = 0;
+            }
+        }
+    }
+
+    template<typename U>
+    Matrix(const vector<vector<U>> &v) {
+        init(v);
+    }
+
+    template<typename U>
+    void init(const vector<vector<U>> &v) {
+        assert(v.size() == N);
+
+        for(int i=0;i<N;i++) {
+            assert(v[i].size() == N);
+
+            for(int j=0;j<N;j++) {
+                values[i][j] = v[i][j];
+            }
+        }
+    }
 
     array<T,N>& operator[](int index) {
         assert(index >= 0 && index < N);
         return values[index];
     }
 
-    void print() {
+    const array<T,N>& operator[](int index) const {
+        assert(index >= 0 && index < N);
+        return values[index];
+    }
+
+    void make_identity() {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                values[i][j] = i == j ? 1 : 0;
+    }
+
+    Matrix operator*(const Matrix &other) const {
+        Matrix product;
+
         for(int i=0;i<N;i++) {
-            cerr << "[";
+            for(int k=0;k<N;k++) {
+                uint64_t result = 0;
+ 
+                for(int j=0;j<N;j++) {
+                    result += uint64_t(values[i][j]) * uint64_t(other[j][k]);
+                    if(result > CUTOFF) {
+                        product[i][k] += result;
+                        result = 0;
+                    }
+                }
+                product[i][k] += result;
+            }
+        }
+        
+
+        return product;
+    }
+
+    Matrix& operator*=(const Matrix &other) {
+        return *this = *this * other;
+    }
+
+    Matrix operator+(const Matrix &other) const {
+        Matrix addition;
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                addition[i][j] = values[i][j] + other[i][j];
+            }
+        }
+        return addition;
+    }
+
+    Matrix& operator+=(const Matrix &other) {
+        return *this = *this + other;
+    }
+
+    Matrix operator-(const Matrix &other) const {
+        Matrix subtraction;
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                subtraction[i][j] = values[i][j] - other[i][j];
+            }
+        }
+        return subtraction;
+    }
+
+    Matrix& operator-=(const Matrix &other) {
+        return *this = *this - other;
+    }
+
+    void print() const {
+        cerr << "[";
+        for(int i=0;i<N;i++) {
+            if(i == 0)
+                cerr << "[";
+            else
+                cerr <<" [";
             for(int j=0;j<N;j++) {
                 cerr << values[i][j];
                 if(j != N-1) {
                     cerr << ", ";
                 }
             }
-            cerr << "]\n";
+            if(i != N-1)
+                cerr << "],\n";
+            else
+                cerr << "]";
         }
-    }
-
-    void identity() {
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                values[i][j] = 0;
-            }
-        }
-
-        for(int i=0;i<N;i++) {
-            values[i][i] = 1;
-        }
-    }
-
-    Matrix& operator*=(Matrix other) {
-        Matrix<T, N> result;
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                for(int k=0;k<N;k++) {
-                    result[i][k] += values[i][j] * other[j][k];
-                }
-            }
-        }
-        values = result.values;
-        return *this;
-    }
-
-    Matrix& operator+=(Matrix other) {
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                values[i][j] += other.values[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Matrix& operator-=(Matrix other) {
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                values[i][j] -= other.values[i][j];
-            }
-        }
-        return *this;
-    }
+        cerr << "]\n";
+    }    
 };
 
 template<typename T, int N>
-Matrix<T, N> operator*(Matrix<T, N> left, Matrix<T, N> const & right) {
-    left *= right;
-    return left;
-}
-
-template<typename T, int N>
-Matrix<T, N> operator+(Matrix<T, N> left, Matrix<T, N> const & right) {
-    left += right;
-    return left;
-}
-
-template<typename T, int N>
-Matrix<T, N> operator-(Matrix<T, N> left, Matrix<T, N> const & right) {
-    left -= right;
-    return left;
-}
-
-template<typename T, int N>
-Matrix<T, N> power(Matrix<T, N> a, int64_t expo) {
-    assert(expo >= 0); // exponent must be >= 0
+Matrix<T, N> power(Matrix<T, N> &a, int64_t p) {
+    assert(p >= 0); // exponent must be >= 0
     
+    Matrix<T, N> m = a;
     Matrix<T, N> result;
-    result.identity();
+    result.make_identity();
 
-    while(expo > 0) {
-        if(expo & 1) {
-            result *= a;
+    while(p > 0) {
+        if(p & 1) {
+            result *= m;
         }
-        a *= a;
-        expo /= 2;
+
+        p /= 2;
+
+        if(p > 0)
+            m *= m;
     }
     return result;
 }
 
-template<typename U, int N>
-Matrix<U, N> gp_series(Matrix<U, N>& a, int64_t n) {
-    Matrix<U, N> result;
-    Matrix<U, N> result1;
-    Matrix<U, N> I;
-    result1.identity();
-    I.identity();
+template<typename T, int N>
+Matrix<T, N> gp_series_sum(Matrix<T, N> &a, int64_t n) {
+    Matrix<T, N> result;
+    Matrix<T, N> result1;
+    Matrix<T, N> I;
+    result1.make_identity();
+    I.make_identity();
     vector<int64_t> pows;
     while(n > 0) {
         pows.push_back(n);
---
+++ GetGood/templates/ankur/numeric/new_mint.cpp
@@ -0,0 +1,208 @@
+template <typename T>
+class Modular {
+public:
+    using Type = typename decay<decltype(T::value)>::type;
+
+    constexpr Modular() : value() {}
+    template <typename U>
+    Modular(const U& x) {
+        value = normalize(x);
+    }
+
+    template <typename U>
+    static Type normalize(const U& x) {
+        Type v;
+        if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+        else v = static_cast<Type>(x % mod());
+        if (v < 0) v += mod();
+        return v;
+    }
+
+    const Type& operator()() const { return value; }
+    template <typename U>
+    explicit operator U() const { return static_cast<U>(value); }
+    constexpr static Type mod() { return T::value; }
+
+    Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+    Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+    template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+    template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+    Modular& operator++() { return *this += 1; }
+    Modular& operator--() { return *this -= 1; }
+    Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+    Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+    Modular operator-() const { return Modular(-value); }
+
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+#ifdef _WIN32
+        uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+        uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+        asm(
+          "divl %4; \n\t"
+          : "=a" (d), "=d" (m)
+          : "d" (xh), "a" (xl), "r" (mod())
+        );
+        value = m;
+#else
+        value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+        return *this;
+    }
+
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+        int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+        value = normalize(value * rhs.value - q * mod());
+        return *this;
+    }
+
+    template <typename U = T>
+    typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+        value = normalize(value * rhs.value);
+        return *this;
+    }
+
+    static const int SAVE_INV = int(1e6) + 5;
+    static Modular save_inv[SAVE_INV];
+
+    static void prepare_inv() {
+        // Make sure MOD is prime, which is necessary for the inverse algorithm below.
+        for (int64_t p = 2; p * p <= mod(); p += p % 2 + 1)
+            assert(mod() % p != 0);
+ 
+        save_inv[0] = 0;
+        save_inv[1] = 1;
+ 
+        for (int i = 2; i < SAVE_INV; i++)
+            save_inv[i] = save_inv[mod() % i] * (mod() - mod() / i);
+    }
+
+    Modular inv() const {
+        if (save_inv[1] == 0)
+            prepare_inv();
+ 
+        if (value < SAVE_INV)
+            return save_inv[value];
+ 
+        Modular product = 1;
+        int v = value;
+ 
+        while (v >= SAVE_INV) {
+            product *= mod() - mod() / v;
+            v = mod() % v;
+        }
+ 
+        return product * save_inv[v];
+    }
+
+    // some optimisation
+    Modular& operator/=(const Modular& other) { return *this *= other.inv(); }
+
+    template <typename U>
+    friend const Modular<U>& abs(const Modular<U>& v) { return v; }
+
+    template <typename U>
+    friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
+
+private:
+    Type value;
+};
+
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+    if(b < 0) {
+        return power(a.inv(), -b);
+    }
+    Modular<T> x = a, res = 1;
+    U p = b;
+    while (p > 0) {
+        if (p & 1) res *= x;
+        x *= x;
+        p >>= 1;
+    }
+    return res;
+}
+
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+    return number() == 0;
+}
+
+template <typename T>
+string to_string(const Modular<T>& number) {
+    return to_string(number());
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+    return stream << number();
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+    typename common_type<typename Modular<T>::Type, int64_t>::type x;
+    stream >> x;
+    number.value = Modular<T>::normalize(x);
+    return stream;
+}
+
+/*
+using ModType = int;
+
+struct VarMod { static ModType value; };
+ModType VarMod::value;
+ModType& md = VarMod::value;
+using Mint = Modular<VarMod>;
+*/
+
+constexpr int md = @@HERE@@
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+template<typename T> Modular<T> Modular<T>::save_inv[Modular<T>::SAVE_INV];
+
+vector<Mint> fact(1, 1);
+vector<Mint> inv_fact(1, 1);
+
+Mint C(int n, int k) {
+    if (k < 0 || k > n) {
+        return 0;
+    }
+    while ((int) fact.size() < n + 1) {
+        fact.push_back(fact.back() * (int) fact.size());
+        inv_fact.push_back(1 / fact.back());
+    }
+    return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
