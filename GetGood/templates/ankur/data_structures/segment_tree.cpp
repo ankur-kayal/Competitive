6691ab97820d96843e9d725b1fd7bdea1846d488
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-02 23:17:26 +0530

    Templates upgraded

--- GetGood/templates/ankur/data_structures/segment_tree.cpp
+++ GetGood/templates/ankur/data_structures/segment_tree.cpp
@@ -1,156 +1,153 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define Stringize( L )     #L 
-#define MakeString( M, L ) M(L)
-#define $Line MakeString( Stringize, __LINE__ )
-#define Reminder __FILE__ "(" $Line ") : Warning: "
+struct segment_change {
+    int64_t to_set;
+    static const int64_t SENTINEL = -1;
 
-struct custom_node {
-    int64_t val;
-    int64_t lazy;
-    // add more data types if you need  
+    // Make sure that the default constructor is the identity segment_change
+    segment_change(int64_t _to_set = SENTINEL) : to_set(_to_set) {}
 
-    custom_node() {
-        ... init here with default neutral values;
-
-        /*
-        val = 1e18;
-        lazy = 1e18;
-        */
+    // resets to the SENTINEL value
+    void reset() {
+        to_set = SENTINEL;
     }
 
-    custom_node(int64_t _val) {
-        ... init here with the actual value
-        /*
-        val = _val;
-        lazy = 1e18;
-        */
+    bool has_change() const {
+        return to_set != SENTINEL;
     }
 
-    custom_node op(custom_node& other) {
-        ... update the operation you want to use
-        /*
-        custom_node ans = custom_node(min(val, other.val));
-        return ans;
-        */
+    // Return the combined result of applying this segment change followed by the `other`.
+    segment_change combine(const segment_change &other) const {
+        return segment_change(other.to_set);
+    }
+};
+
+struct segment {
+    int64_t sum;
+
+    // Make sure that the default constructor is the identity segment
+    segment(int64_t _sum = 0) : sum(_sum) {}
+
+    // checks if the current segment is empty based on the default value
+    bool empty() const {
+        return sum == 0;
     }
 
-    custom_node lazy_op(int64_t val, int64_t size) {
-        ... define the lazy operation you want to use
-        /*
-        custom_node ans;
-        ans.lazy = val;
-        ans.val = (val);
-        */
+    // evaluating the change on a particular node
+    void apply(int length, const segment_change &change) {
+        if(change.has_change()) {
+            sum = change.to_set;
+        }
+    }
+
+    // merge function
+    void join(const segment &other) {
+        sum = sum + other.sum;
+    }
+
+    void join(const segment &a, const segment &b) {
+        *this = a;
+        join(b);
     }
 };
 
 struct segment_tree {
-    int size; // size of the segment tree, 
+    int tree_n = 0;
+    vector<segment> tree;
+    vector<segment_change> changes;
 
-    custom_node NEUTRAL_ELEMENT; //  neutral element
-
-    int NO_LAZY; // define the parameter which defines if you have any lazy left
-
-    vector<custom_node> values; // segment tree cumulative values
+    segment_tree(int n = -1) {
+        if(n >= 0) {
+            init(n);
+        }
+    }
 
     void init(int n) {
-        #pragma message(Reminder "Don't forget to define the lazy parameter")
-        NO_LAZY = -1; // define the no lazy parameter
-        size = 1;
-        while(size < n) {
-            size *= 2;
+        tree_n = 1;
+        while(tree_n < n) {
+            tree_n *= 2;
         }
-        values.assign(2 * size, NEUTRAL_ELEMENT);
+
+        tree.assign(2 * tree_n, segment());
+        changes.assign(2 * tree_n, segment_change());
     }
 
-    custom_node merge(custom_node& a, custom_node& b) {
-        custom_node res;
-        res = a.op(b);
-        return res;
-    }
-
-    void build(vector<int64_t>& a, int x, int lx, int rx) {
+    void build(const vector<segment> &initial, int x, int lx, int rx) {
         if(rx - lx == 1) {
-            if(lx < int(a.size())) {
-                values[x] = custom_node(a[lx]);
+            if(lx < int(initial.size())) {
+                tree[x] = initial[lx];
             }
             return;
         }
 
         int m = (rx + lx) / 2;
-        build(a, 2 * x + 1, lx, m);
-        build(a, 2 * x + 2, m, rx);
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
+        build(initial, 2 * x + 1, lx, m);
+        build(initial, 2 * x + 2, m, rx);
+        tree[x].join(tree[2 * x + 1], tree[2 * x + 2]);
     }
 
-    void build(vector<int64_t>& a) {
-        build(a, 0, 0, size);
+    // Builds our tree from an array in O(n).
+    void build(const vector<segment> &initial) {
+        int n = int(initial.size());
+        init(n);
+        assert(n <= tree_n);
+
+        build(initial, 0, 0, tree_n);
     }
 
-    custom_node query(int l, int r, int x, int lx, int rx) {
+    // Apply the change to the current index and push down the change
+    void propagate(int x, int lx, int rx) {
+        if(!changes[x].has_change()) {
+            return;
+        }
+        tree[x].apply(rx - lx, changes[x]);
+        if(rx - lx != 1) {
+            changes[2 * x + 1] = changes[2 * x + 1].combine(changes[x]);
+            changes[2 * x + 2] = changes[2 * x + 2].combine(changes[x]);
+        }
+        changes[x].reset();
+    }
+
+    // Queries in the range [l, r)
+    segment query(int l, int r, int x, int lx, int rx) {
         propagate(x, lx, rx);
         if(lx >= r or l >= rx) {
-            return NEUTRAL_ELEMENT;
+            return segment();
         }
-
-        else if(lx >= l and rx <= r) {
-            return values[x];
+        if(lx >= l && rx <= r) {
+            return tree[x];
         }
-
         int m = (lx + rx) / 2;
-
-        custom_node s1 = query(l, r, 2 * x + 1, lx, m);
-        custom_node s2 = query(l, r, 2 * x + 2, m, rx);
-        return merge(s1, s2);
+        segment left = query(l, r, 2 * x + 1, lx, m);
+        segment right = query(l, r, 2 * x + 2, m, rx);
+        left.join(right);
+        return left;
     }
 
-    custom_node query(int l, int r) {
-        return query(l, r, 0, 0, size);
+    segment query(int l, int r) {
+        return query(l, r, 0, 0, tree_n);
     }
 
-    void propagate(int x, int lx, int rx) {
-        if(values[x].lazy == NO_LAZY) {
+    // Updates the change in the range [l, r)
+    void update(int l, int r, const segment_change &change, int x, int lx, int rx) {
+        propagate(x, lx, rx);
+        if(lx >= r || l >= rx) {
             return;
         }
-        int m = (rx - lx) / 2;
-
-        values[x] = values[x].lazy_op(values[x].lazy, rx - lx);
-
-        if(rx - lx != 1) { // if you have children propagate the lazy parameter to the children
-            // custom define how you want to update !! Most errors occur Here !!
-            #pragma message(Reminder "Define how you want to pass lazy parameters, once done comment this")
-            values[2 * x + 1].lazy = values[x].lazy;
-            values[2 * x + 2].lazy = values[x].lazy;
-
-        }
-
-        values[x].lazy = NO_LAZY;
-    }
-
-    void range_update(int l, int r, int val, int x, int lx, int rx) {
-        propagate(x, lx, rx); // try propagating once you touch every node
-        if(lx >= r or l >= rx) {
-            return;
-        }
-        if(lx >= l and rx <= r) {
-            // custom define how you want to update !! Most errors occur Here !!
-            #pragma message(Reminder "Define how you want to pass lazy parameters and update, once done comment this")
-            // values[x].val = val;
-            values[x].lazy = val;
+        if(lx >= l && rx <= r) {
+            changes[x] = changes[x].combine(change);
             propagate(x, lx, rx);
             return;
         }
 
         int m = (lx + rx) / 2;
-        range_update(l, r, val, 2 * x + 1, lx, m);
-        range_update(l, r, val, 2 * x + 2, m, rx);
-
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
+        update(l, r, change, 2 * x + 1, lx, m);
+        update(l, r, change, 2 * x + 2, m, rx);
+        tree[x].join(tree[2 * x + 1], tree[2 * x + 2]);
     }
 
-    void range_update(int l, int r, int val) {
-        range_update(l, r, val, 0, 0, size);
-    }
+    void update(int l, int r, const segment_change &change) {
+        update(l, r, change, 0, 0, tree_n);
+    } 
 };
